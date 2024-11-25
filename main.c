5706ca8a300c8bccf80228371525773ed9d1d1be
Yuri Schaeffer <yuri@schaeffer.tk>
2024-11-25 08:23:38 +0100

    Recieve RABI output!

--- main.c
+++ main.c
@@ -28,11 +28,34 @@ sin_xsp(uint x, uint scale, float phase)
 void pattern(uint n, uint t)
 {
     for (uint i = 0; i < n; ++i) {
-        uint8_t r = sin_xsp(t, 9, M_PI*2*(i+0)/n);
+        uint8_t r = sin_xsp(t, 10, M_PI*2*(i+0)/n);
         uint8_t g = sin_xsp(t, 10, M_PI*2*(i+2)/n);
-        uint8_t b = sin_xsp(t, 11, M_PI*2*(i+4)/n);
+        uint8_t b = sin_xsp(t, 10, M_PI*2*(i+4)/n);
 
         put_pixel(r,g,b);
+    }
+}
+
+void gpio_callback(uint gpio, uint32_t events)
+{
+    static absolute_time_t t_fall, t_rise;
+    if (gpio != 3) return;
+
+    if (events & 0x8) {
+        //rising edge
+        t_rise = get_absolute_time();
+
+        if (t_rise - t_fall > 80000) printf("\n");
+
+    } else if (events & 0x4) {
+        //falling edge
+        t_fall = get_absolute_time();
+
+        if (t_fall - t_rise < 4500) {
+            printf("0");
+        } else {
+            printf("1");
+        }
     }
 }
 
@@ -42,12 +65,12 @@ int main()
 
     uint offset = pio_add_program(pio0, &ws2812_program);
     ws2812_program_init(pio0, 0, offset, LED_OUT_PIN, 800000, IS_RGBW);
+    gpio_set_irq_enabled_with_callback(3, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
     unsigned int t = 0;
     while (1) {
         pattern(W, t);
         sleep_ms(100);
         t++;
-        printf("hello\n");
     }
 }
