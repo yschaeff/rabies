12ffe10a629a4a0c5b2c6ed59964c05cf5bdd919
Yuri Schaeffer <yuri@schaeffer.tk>
2024-12-02 09:03:12 +0100

    Double buffering for key input as not to get get spurious key events

--- main.c
+++ main.c
@@ -25,26 +25,32 @@
 #define W 9
 
 uint8_t led_states[W];
-uint8_t key_states[W];
-
-uint8_t lin_lut[] = { 0, 1, 2, 3, 4, 5, 7, 9,12, 15, 18, 22, 27, 32, 38, 44, 51, 58, 67, 76, 86, 96, 108, 120, 134, 148, 163, 180, 197, 216, 235, 255 };
+uint8_t *key_states_read;
+uint8_t *key_states_write;
+uint8_t key_states_a[W];
+uint8_t key_states_b[W];
 
 void update_leds(uint n)
 {
     for (uint i = 0; i < n; ++i) {
-        if (key_states[i]) {
+        if (key_states_read[i]) {
             led_states[i] = 0xFF;
+        } else {
+            led_states[i] >>= 1;
         }
-        /*else {*/
-            /*led_states[i] = 0x00;*/
-
-        /*}*/
         uint32_t grba = led_states[i]<<16;
-        /*uint32_t grba = lin_lut[led_states[i]]<<16;*/
-
         pio_sm_put_blocking(WS_PIO, WS_SM, grba);
-        /*if (led_states[i]) led_states[i]--;*/
-        led_states[i] >>= 1;
+    }
+}
+
+void flip()
+{
+    if (key_states_read == key_states_a) {
+        key_states_read  = key_states_b;
+        key_states_write = key_states_a;
+    } else {
+        key_states_read  = key_states_a;
+        key_states_write = key_states_b;
     }
 }
 
@@ -65,6 +71,9 @@ void setup()
     //statemachine 1 initiates cry when data in TX queue
     uint rb_howl_addr = pio_add_program(RB_PIO, &howl_start_program);
     howl_start_program_init(RB_PIO, RB_HOWL_SM, rb_howl_addr, KEY_OUT_PIN, 500);
+
+    //Make sure both buffers point to something
+    flip();
 }
 
 bool statemachine(bool bit, bool reset)
@@ -89,11 +98,11 @@ bool statemachine(bool bit, bool reset)
             k = K;
             state = bit + 2;
             w++;
-            key_states[w] = 0;
+            /*key_states_write[w] = 0;*/
             return false;
         case 2:                     //read 1 of K bits
-            key_states[w] <<= 1;
-            key_states[w] |= bit;
+            key_states_write[w] <<= 1;
+            key_states_write[w] |= bit;
             state -= !--k;
             return false;
         case 3:                     //end ow howl
@@ -114,7 +123,7 @@ int main()
     while (1) {
         absolute_time_t t_now = get_absolute_time();
 
-        if (howl_done && t_next_led < t_now) {
+        if (t_next_led < t_now) {
             t_next_led = t_now + 50000;
             update_leds(W);
         }
@@ -130,7 +139,8 @@ int main()
             pio_sm_put_blocking(RB_PIO, RB_HOWL_SM, 1);
             t_last_bit = t_now;
             howl_done = false;
-            /*update_leds(W);*/
+            flip();
+            memset(key_states_write, 0, W);
         }
 
         if (!pio_sm_is_rx_fifo_empty(RB_PIO, RB_LISTEN_SM)) {
