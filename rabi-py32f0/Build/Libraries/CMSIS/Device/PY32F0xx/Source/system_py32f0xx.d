dbfc8b08de84e3930087d0a0201c3ac2d7d398e8
Yuri Schaeffer <yuri@schaeffer.tk>
2025-02-27 22:07:43 +0100

    Move Puya stuff to their own directory to prepare for upcoming merge.

---
+++ rabi-py32f0/Build/Libraries/CMSIS/Device/PY32F0xx/Source/system_py32f0xx.d
@@ -0,0 +1,18 @@
+Build/Libraries/CMSIS/Device/PY32F0xx/Source/system_py32f0xx.o: \
+ Libraries/CMSIS/Device/PY32F0xx/Source/system_py32f0xx.c \
+ Libraries/CMSIS/Device/PY32F0xx/Include/py32f0xx.h \
+ Libraries/CMSIS/Device/PY32F0xx/Include/py32f002ax5.h \
+ Libraries/CMSIS/Core/Include/core_cm0plus.h \
+ /usr/lib/gcc/arm-none-eabi/14.2.1/include/stdint.h \
+ Libraries/CMSIS/Core/Include/cmsis_version.h \
+ Libraries/CMSIS/Core/Include/cmsis_compiler.h \
+ Libraries/CMSIS/Core/Include/cmsis_gcc.h \
+ Libraries/CMSIS/Device/PY32F0xx/Include/system_py32f0xx.h
+Libraries/CMSIS/Device/PY32F0xx/Include/py32f0xx.h:
+Libraries/CMSIS/Device/PY32F0xx/Include/py32f002ax5.h:
+Libraries/CMSIS/Core/Include/core_cm0plus.h:
+/usr/lib/gcc/arm-none-eabi/14.2.1/include/stdint.h:
+Libraries/CMSIS/Core/Include/cmsis_version.h:
+Libraries/CMSIS/Core/Include/cmsis_compiler.h:
+Libraries/CMSIS/Core/Include/cmsis_gcc.h:
+Libraries/CMSIS/Device/PY32F0xx/Include/system_py32f0xx.h:
